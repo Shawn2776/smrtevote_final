38125a13d9db33c0da741c657047447e44d4ff81
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-05-16 13:08:57 -0700

    first update to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,12 @@
-import { Roboto } from "next/font/google";
-
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import LoginButton from "@/components/auth/login-button";
 
-const font = Roboto({ subsets: ["latin"], weight: ["400"] });
-
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-sky-500">
+    <main className="flex flex-col items-center justify-center h-full bg-sky-500">
       <div className="space-y-6">
-        <h1 className={cn("text-4xl font-bold", font.className)}>
-          Welcome to SMRTeVote
-        </h1>
-        <p className="text-lg text-gray-600">
-          Get started by editing{" "}
-          <code className="bg-gray-100 p-1 rounded-md">app/page.tsx</code>
-        </p>
+        <h1 className="text-4xl font-bold">Welcome to SMRTeVote</h1>
+
         <LoginButton>
           <Button size="lg">Get started</Button>
         </LoginButton>
